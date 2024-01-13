b9683be574aae76502ae1d77882c98f7f26b9c07
matthkorus <130331198+matthkorus@users.noreply.github.com>
2024-01-13 09:38:40 -0800

    Initial Commit

---
+++ SerpApiTestApp/SerpApiTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SerpApiTestAppApp.swift
+//  SerpApiTestApp
+//
+//  Created by Matt Korus on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SerpApiTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
